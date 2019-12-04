ea0bf60511abc415595d4ca661554aeb6ab21f76
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-04 11:23:21 +0100

    correction

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
-$age = 13;
-$gender = 'Femme';
+$age = 23;
+$gender = 'Homme';
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
-    <meta charset="utf-8" />
-    <title>PHP Partie2 exo2</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8" />
+  <title>PHP Partie2 exo2</title>
+</head>
+<body>
+  <p>
     <?php
-      if ($age < 18 && $gender == 'Homme') {
-        echo 'Vous êtes un homme et vous êtes mineur';
-      } elseif ($gender == 'Homme'){
-        echo 'Vous êtes un homme et vous êtes majeur';
-      } elseif ($age < 18) {
-        echo 'Vous êtes une femme et vous êtes mineure';
-      } else {
-        echo 'Vous êtes une femme et vous êtes majeure';
-      }
+    /* penser à créer les conditions idéales pour une meilleure utilisation client
+    avec les bons messages d'erreur pour éguiller l'utilisateur, ici deux
+    conditions d'erreur, si l'age n'est pas bon ou si le genre n'est pas bon */
+    if($age > 0 && $age < 100){
+      if($age < 18 && $gender == 'Homme'){
+        ?>
+        Vous êtes un homme mineur
+      <?php } elseif($age >= 18 && $gender == 'Homme'){ ?>
+        Vous êtes un homme majeur
+      <?php } elseif($age < 18 && $gender == 'Femme'){ ?>
+        Vous êtes une femme mineure
+      <?php } elseif($age >= 18 && $gender == 'Femme'){ ?>
+        Vous êtes une femme majeure
+      <?php } else { ?>
+        Genre non valide
+      <?php }
+    } else{
       ?>
-  </body>
+      Age non valide
+    <?php } ?>
+  </p>
+</body>
 </html>
